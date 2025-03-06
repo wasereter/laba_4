0a014b0e07b78e7689b0bda1f6b3601eaa1b825d
wasereter <waser0712@gmail.com>
2025-03-06 13:13:08 +0300

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,27 @@
+class Position:
+    position = None
+    
+    def __init__(self, position):
+        self.position = position
+        
+class Department:
+    department = None
+    
+    def __init__(self, department):
+        self.department = department
+
+
+class User :
+  name = None
+  position = None
+  department = None
+
+  def __init__(self,name, position, department):
+    self.name = name
+    self.position = position
+    self.department = department
+    
+position = Position('fdsfsdf')
+department = Department('dfsfsdfs')
+user = User('Maks', position, department)
+print(user.position.name)
